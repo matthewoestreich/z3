50c87724a0884aa72392656d9f8afef9bfdbc3ba
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-02 11:12:07 -0500

    update

--- cdn/z3-built.js
+++ cdn/z3-built.js
@@ -841,6 +841,7 @@ function createExportWrapper(name, nargs) {
 var wasmBinaryFile;
 
 function findWasmBinary() {
+  console.log("from z3-built on CDN", { inFn: "findWasmBinary" });
     //return locateFile('z3-built.wasm');
     return locateFile("https://z3-tawny.vercel.app/z3-built.wasm");
 }
